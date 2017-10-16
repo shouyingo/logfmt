65b4804d7b0952e12c795afc666a918829c41f47
vizee <vizee@live.cn>
2017-10-16 23:35:58 +0800

    add ms part

--- logfmt.go
+++ logfmt.go
@@ -5,12 +5,11 @@ import (
 	"fmt"
 	"sort"
 
-	"github.com/Sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 )
 
 var (
-	SortKeys   = false
-	IgnoreData = false
+	SortKeys = false
 )
 
 var (
@@ -35,42 +34,25 @@ func (*PlainFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 	}
 
 	// time
-	var tbuf [20]byte
-	year, month, day := entry.Time.Date()
-	tbuf[0], tbuf[1] = getdigit(byte(year / 100))
-	tbuf[2], tbuf[3] = getdigit(byte(year % 100))
-	tbuf[4] = '-'
-	tbuf[5], tbuf[6] = getdigit(byte(month))
-	tbuf[7] = '-'
-	tbuf[8], tbuf[9] = getdigit(byte(day))
-	tbuf[10] = '/'
-	hour, min, sec := entry.Time.Clock()
-	tbuf[11], tbuf[12] = getdigit(byte(hour))
-	tbuf[13] = ':'
-	tbuf[14], tbuf[15] = getdigit(byte(min))
-	tbuf[16] = ':'
-	tbuf[17], tbuf[18] = getdigit(byte(sec))
-	tbuf[19] = ' '
-	buf.Write(tbuf[:])
+	var t [32]byte
+	buf.Write(entry.Time.AppendFormat(t[:0], "2006-01-02/15:04:05.000 "))
 
 	// level
 	buf.WriteString(tags[entry.Level])
 
-	if !IgnoreData && len(entry.Data) > 0 {
+	if len(entry.Data) > 0 {
 		// fields
 		buf.WriteByte('{')
 		if !SortKeys {
 			more := false
 			for k, v := range entry.Data {
 				if more {
-					buf.WriteByte(',')
-					buf.WriteByte(' ')
+					buf.WriteString(", ")
 				} else {
 					more = true
 				}
 				buf.WriteString(k)
-				buf.WriteByte(':')
-				buf.WriteByte(' ')
+				buf.WriteString(": ")
 				fmt.Fprint(buf, v)
 			}
 		} else {
@@ -85,19 +67,16 @@ func (*PlainFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 			more := false
 			for _, k := range keys {
 				if more {
-					buf.WriteByte(',')
-					buf.WriteByte(' ')
+					buf.WriteString(", ")
 				} else {
 					more = true
 				}
 				buf.WriteString(k)
-				buf.WriteByte(':')
-				buf.WriteByte(' ')
+				buf.WriteString(": ")
 				fmt.Fprint(buf, entry.Data[k])
 			}
 		}
-		buf.WriteByte('}')
-		buf.WriteByte(' ')
+		buf.WriteString("} ")
 	}
 
 	// message
@@ -105,8 +84,4 @@ func (*PlainFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 
 	buf.WriteByte('\n')
 	return buf.Bytes(), nil
-}
-
-func getdigit(n byte) (byte, byte) {
-	return n/10 + '0', n%10 + '0'
 }
